c50090c8a5fd3006ae505db64841a5052704de60
koridoc <koridoc@ya.ru>
2018-10-09 22:07:45 +0500

    lab3 v1.0.0 Динамиеские массивы

--- lab3/Array.h
+++ lab3/Array.h
@@ -4,20 +4,44 @@ template<typename T>
 struct Array{
 	size_t capasity,
 		size;
-	T data[30];
+	T *data;
 
 	Array();
+	~Array();
 	T& operator[](const int id);
 	int insert(T &node);
 	int earase(const int id);
+	void realloc(int newSize);
 	void clear();
 };
 
 template<typename T>
 Array<T>::Array() {
-	capasity = 30;
+	data = nullptr;
+	capasity = 4;
 	size = 0;
+	this->realloc(capasity);
 }
+template<typename T>
+Array<T>::~Array(){
+	delete[] data;
+}
+
+template<typename T>
+void Array<T>::realloc(int newCapasity){
+	T *newArray = new T[newCapasity];
+
+	for (int i = 0; i < min(this->size, newCapasity); i++)
+		newArray[i] = data[i];
+
+	if(this->data != nullptr)
+		delete[] this->data;
+
+	this->capasity = newCapasity;
+	this->data = newArray;
+};
+
+
 template<typename T>
 T& Array<T>::operator[](const int id){
 	return data[id - 1];
@@ -26,6 +50,10 @@ T& Array<T>::operator[](const int id){
 template<typename T>
 int Array<T>::insert(T &node) {
 	T tmp = node;
+
+	if (size == capasity)
+		this->realloc(2 * size);
+
 	if (size < capasity) {
 		data[size] = tmp;
 		size++;
